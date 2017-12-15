3cf1eba47424d2c417adeb0cfec7c8046e425e27
cnbattle <liqiai@qq.com>
2017-12-15 13:30:20 +0800

    update

--- src/UUID.php
+++ src/UUID.php
@@ -110,7 +110,7 @@ class UUID {
         );
     }
 
-    public static function is_valid($uuid) {
+    private static function is_valid($uuid) {
         return preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
                 '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/i', $uuid) === 1;
     }
