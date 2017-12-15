ff7cf5263e181d5fe28e3e9ab9f99bacb4dcfa0b
cnbattle <liqiai@qq.com>
2017-12-15 14:03:07 +0800

    update

--- src/Func.php
+++ src/Func.php
@@ -21,6 +21,34 @@ class Func
     }
 
     /**
+     * 批量搜索字符串
+     * @param $str 搜索的字符串
+     * @param $startChar 开始字符串
+     * @param $endChar 结束字符串
+     * @return array|string
+     */
+    public static function strSearchAll($str, $startChar,$endChar){
+        $startNum = 0;
+        $endNum = 0;
+        $arr = array();
+        $startCount = substr_count($str, $startChar);// 获取开始字符串 出现的次数
+        $endCount = substr_count($str, $endChar);// 获取结束字符串 出现的次数
+        $endCharNum = strlen($endChar);
+        if ($startCount == $endCount){
+            for($i = 0; $i < $startCount; $i++){
+                $startNum = strpos($str, $startChar, $startNum);
+                $endNum = strpos($str, $endChar, $endNum);
+                $tmp = substr($str,$startNum,$endNum-$startNum+$endCharNum);
+                $arr[] = $tmp;
+                $startNum++;
+                $endNum++;
+            }
+            return $arr;
+        }
+        return '数据不对称';
+    }
+    
+    /**
      * 参数检测
      * @param $arr key数组
      * @param $parameter 检测的数组
