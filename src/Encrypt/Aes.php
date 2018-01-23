5023be014153c7da7db2a06d17fa0f417a04f101
cnbattle <liqiai@qq.com>
2018-01-23 17:08:29 +0800

    update

--- src/Encrypt/Aes.php
+++ src/Encrypt/Aes.php
@@ -10,31 +10,45 @@ namespace PHPBase\Encrypt;
 
 class Aes
 {
-    private $key; // php -r "echo (openssl_random_pseudo_bytes(32));"
-    private $iv; // php -r "echo (openssl_random_pseudo_bytes(16));"
+    private $key;
+    private $iv;
 
-
-    public function __construct($key, $iv) {
-        $this->key = $key;
-        $this->iv = $iv;
-    }
-
-    public function createKey() {
+    /**
+     * 初始化生产 key iv,初次需要保存,以便后续解密
+     * @return array
+     */
+    public function init() {
         $this->key = base64_encode(openssl_random_pseudo_bytes(32));
-        return $this->key;
-    }
-
-    public function createIv() {
         $this->iv = base64_encode(openssl_random_pseudo_bytes(16));
-        return $this->iv;
+        return ['key' => $this->key, 'iv' => $this->iv];
     }
 
+    public function setKey($key) {
+        $this->key = $key;
+        return $this;
+    }
+
+    public function setIv($iv) {
+        $this->iv = $iv;
+        return $this;
+    }
+
+    /**
+     * 加密
+     * @param $data
+     * @return string
+     */
     public function encrypt($data) {
         $encrypted = openssl_encrypt($data, 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
         return base64_encode($encrypted);
 
     }
 
+    /**
+     * 解密
+     * @param $data
+     * @return string
+     */
     public function decrypt($data) {
         $decrypted = openssl_decrypt(base64_decode($data), 'aes-256-cbc', base64_decode($this->key), OPENSSL_RAW_DATA, base64_decode($this->iv));
         return $decrypted;
