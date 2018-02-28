f4616a45eb4b064f9cff30ec38a81330177402c3
cnbattle <liqiai@qq.com>
2018-02-28 12:44:33 +0800

    update

---
+++ src/Sign/Func.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cnbattle
+ * Date: 18-2-28
+ * Time: 上午11:26
+ */
+
+namespace PHPBase\Sign;
+
+class Func
+{
+    /**
+     * 获取安全验证sign
+     * @param $postData
+     * @param string $signKey
+     * @return array
+     */
+    public function getVerifySign($postData,$signKey = 'cnbattle'){
+        ksort($postData);
+        $str = http_build_query($postData);
+        $timestamp = time();
+        $sign = md5($signKey.$str.$timestamp);
+        return ['sign'=>$sign,'timestamp'=>$timestamp];
+    }
+}
